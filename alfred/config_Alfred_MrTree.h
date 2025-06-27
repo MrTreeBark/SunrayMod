a969bb3ac7f2c0b537b26a4f9701a970008fcbb1
MrTreeBark <99759599+MrTreeBark@users.noreply.github.com>
2025-06-27 13:53:53 +0200

    UPDATE Alfred config, Alfred works now if not too close to perimeter
    - bumpy lawn will trigger obstacles -> lift obstacle off
    - faster slow rotation
    
    -->still a bug if undocking and gps reset--> long loop of resetting again and again (not coming from gps reset after undock)
    -->still need to add safety time for obstacles, because now mower will reverse ior forward nfite when he cant move

--- alfred/config_Alfred_MrTree.h
+++ alfred/config_Alfred_MrTree.h
@@ -87,7 +87,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 // should the lift sensor be enabled? (yes: uncomment line, no: comment line)
 #define ENABLE_LIFT_DETECTION  1
 // should the lift sensor be used for obstacle avoidance (if not, mower will simply turn off cutter motor if lifted)
-#define LIFT_OBSTACLE_AVOIDANCE 1  
+//#define LIFT_OBSTACLE_AVOIDANCE 1  
 #define LIFT_INVERT  false       // invert lift sensor state? 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -123,7 +123,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 //rotation speeds, also this is easy to tune for your expectations --> going to be changed to angularRamp() with less parameters
 #define ROTATION_RAMP               true  // uses a ramp for angletotargetfits (very nice), ROTATETOTARGETSPEED´s are disabled if this is true
 #define ROTATION_RAMP_MAX           85   // (deg/s) maximum rotation speed
-#define ROTATION_RAMP_MIN           10    // (deg/s) minimum rotation speed
+#define ROTATION_RAMP_MIN           15    // (deg/s) minimum rotation speed
 #define ROTATETOTARGETSPEED1        65.0  // (deg/s) if angle difference to point is more than ANGLEDIFF1 then this value will be used...   warning, a high value will result in extreme gearmotor stress (test at own risk, 65deg/s is still safe and fast)
 #define ROTATETOTARGETSPEED2        40.0  // (deg/s) if angle difference to point is between ANGLEDIFF1 and ANGLEDIFF2 then this value will be used...
 #define ROTATETOTARGETSPEED3        25.0  // (deg/s) if angle difference to point is less than ANGLEDIFF2 then this value will be used...
@@ -131,7 +131,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 #define ANGLEDIFF2                  15.0  // (deg) if angle to point is between ANGLEDIFF1 and ANGLEDIFF2 --> ROTATETOTARGETSPEED2 will be used, if it is less ROTATETOTARGETSPEED3 will be used...
 #define ANGLEPRECISE                5.0   // (deg) if Angle to point ist within 5deg, mower will continue with Linetracker.cpp Stanleycode and NEARWAYPOINT setup
 #define TRANSITION_ANGLE            25    // (deg) if next point is below this angle, mower will not stop completely but makes a transition with TRANSITION_SPEED
-#define TRANSITION_SPEED            0.10  // (m/s) speed for transition to next point angle
+#define TRANSITION_SPEED            0.15  // (m/s) speed for transition to next point angle
 #define TARGETFITS_ANGLE            35    // (deg/s) if target angle is below that, linetracker uses stanley for tracking. If over that angle, mower will stop and readjust with rotation at it´s current position
 //use a PID controller for mowmotor to set an RPM instead of PWM? If you use this (there will be a console output with data after 10sec when you activate the mowmotor and this is enabled)
 //CONFIG HINT: for the following options it is important if you have mow motor odometrie: USE_MOW_RPM_SET, ADAPTIVE_SPEED_MODE, ESCAPE_LAWN_MODE. If you do not have odometrie: use mode 1 on both cases and set USE_MOW_RPM_SET = false, if you have odometrie use mode 2 on both cases and set USE_MOW_RPM_SET = true (recommended)
